d7956b87e55d93f008e7cb80c4d006a38a9fdde8
Ian van Zyl <no-email@email.com>
2025-05-16 10:46:18 +0200

    Added prisma and tested e2e test

--- apps/backend-e2e/src/backend/backend.spec.ts
+++ apps/backend-e2e/src/backend/backend.spec.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
+import { shared } from '@nextjs-expressjs-postgre-sql/shared';
 
 describe('GET /', () => {
   it('should return a message', async () => {
-    const res = await axios.get(`/`);
+    const res = await axios.get(`/api`);
 
     expect(res.status).toBe(200);
-    expect(res.data).toEqual({ message: 'Hello API' });
+    expect(res.data).toEqual({ message: shared() });
   });
 });
