a272ffd4aa5d850101c2c7cd33704db10128694d
Ian van Zyl <no-email@email.com>
2025-05-27 11:34:56 +0200

    Create default jenkins pipeline

---
+++ jenkins/init.groovy.d/create-job.groovy
@@ -0,0 +1,33 @@
+import jenkins.model.*
+import hudson.plugins.git.*
+import hudson.model.*
+import org.jenkinsci.plugins.workflow.job.*
+import org.jenkinsci.plugins.workflow.cps.*
+import hudson.triggers.Trigger
+
+def jenkins = Jenkins.getInstance()
+def jobName = "nextjs-expressjs-postgreSQL"
+
+if (jenkins.getItem(jobName) == null) {
+    println "--> Creating pipeline job '${jobName}'"
+
+    def job = new WorkflowJob(jenkins, jobName)
+
+    def scm = new GitSCM("https://github.com/IanvanZyl636/nextjs-expressjs-postgreSQL.git")
+    scm.branches = [new BranchSpec("*/master")]
+    scm.userRemoteConfigs = [new UserRemoteConfig("https://github.com/IanvanZyl636/nextjs-expressjs-postgreSQL.git", null, null, null)]
+    scm.extensions = []
+
+    def definition = new CpsScmFlowDefinition(scm, "Jenkinsfile")
+    definition.setLightweight(true)
+
+    job.definition = definition
+
+    jenkins.reload()
+    jenkins.add(job, jobName)
+    job.save()
+
+    println "--> Job '${jobName}' created successfully."
+} else {
+    println "--> Job '${jobName}' already exists. Skipping creation."
+}
