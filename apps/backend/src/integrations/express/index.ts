498f03c0772bdacd53798995249c24d70f8ed8fd
Ian van Zyl <no-email@email.com>
2025-06-02 12:24:47 +0200

    Implement jenkins CI/CD

--- apps/backend/src/integrations/express/index.ts
+++ apps/backend/src/integrations/express/index.ts
@@ -9,12 +9,16 @@ export const initializeExpress = async () => new Promise<void>(resolve => {
 
   app.use(express.json());
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.get('/swagger.json', (_, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+  });
   app.use('/api', f1Router);
   app.use(errorLogger);
 
   const port = process.env.PORT || 3333;
   const server = app.listen(port, async ()=> {
-    console.log(`🚀 Server started!!! Check for api info on http://localhost:${port}/api-docs`);
+    console.log(`🚀 Server started!!! Check for api info on http://localhost:${port}/api-docs or ingest openapi spec on http://localhost:${port}/swagger.json`);
   });
   server.on('error', console.error);
   resolve();
